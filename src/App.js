90b768d4ba0a62642cfbc3781149de1ebc487ac6
FreezeByte <rosas.ruizlg@gmail.com>
2020-04-17 17:25:59 -0500

    consultando API y guardando en componente

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, { Fragment } from 'react';
 import Header from './components/Header';
+import Formulario from './components/Formulario';
+
+import CategoriasProvider from './context/CategoriasContext';
+import RecetasProvider from './context/RecetasContext';
 
 function App() {
   return (
-    <Fragment>
+    <CategoriasProvider>
+      <RecetasProvider>
         <Header
 
         />
+        <div className="container mt-5">
+          <div className="row">
+            <Formulario
 
-    </Fragment>
+            />
+          </div>
+        </div>
+      </RecetasProvider>
+    </CategoriasProvider>
   );
 }
 
